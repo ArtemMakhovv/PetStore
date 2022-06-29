ec8b2560f962ca7ff198abc75d7bc2a5ddb25288
ArtemMakhovv <artem.makhovv@gmail.com>
2022-06-29 23:03:45 +0400

    First

--- src/test/java/com/junit/PetsTests.java
+++ src/test/java/com/junit/PetsTests.java
@@ -1,4 +1,12 @@
 package com.junit;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PetsTests {
+    @Test
+    void test() {
+        assertTrue(false);
+    }
 }
